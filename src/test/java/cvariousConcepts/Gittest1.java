c9ba7e36cb2f08829070804f717e248806cea71a
Ruby K <rubykdec22@gmail.com>
2023-04-18 21:42:29 -0500

    2nd commit

---
+++ src/test/java/cvariousConcepts/Gittest1.java
@@ -0,0 +1,50 @@
+package cvariousConcepts;
+
+import org.openqa.selenium.WebDriver;
+
+public class Gittest1 {
+
+	
+public  void gittest() {
+		
+	WebDriver driver1;
+		
+	        launchBrowser();
+			positiveLoginTest();
+			tearDown();
+			
+			launchBrowser();
+			positiveLoginTest();
+			tearDown();
+			
+			launchBrowser();
+			negLoginTest1();
+			tearDown();
+			
+			launchBrowser();
+			negLoginTest1();
+			tearDown();
+
+	}
+
+		private static void negLoginTest1() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		private static void tearDown() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		private static void positiveLoginTest() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		private static void launchBrowser() {
+			// TODO Auto-generated method stub
+			
+		}
+
+}
